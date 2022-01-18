6c9d2826a789ad01d5966233517a679ad9452a5e
Kayki <grcdjkayki@gmail.com>
2022-01-18 20:20:33 -0300

    adicionado novos cactus, removido erro de colisão

--- script/main.js
+++ script/main.js
@@ -1,10 +1,29 @@
-const out = (out) => {
-    document.querySelector(".out").innerHTML = JSON.stringify(out);
-};
-
 const game = document.getElementById("game");
 const dino = document.getElementById("dino");
-const cactus = document.getElementById("cactus");
+
+let isGameOver = false;
+
+const getCactus = () => {
+    return Array.from(document.getElementsByClassName("cactus"));
+};
+
+const placeCactus = () => {
+    if (isGameOver) {
+        return;
+    }
+    const cactusType = parseInt(Math.random() * (3 - 1) + 1);
+    const cactusId = crypto.randomUUID();
+    const cactus = document.createElement("div");
+    cactus.classList.add("cactus", "move", "cactus-" + cactusType);
+    cactus.id = cactusId;
+    game.appendChild(cactus);
+    setTimeout(() => {
+        if (isGameOver) {
+            return;
+        }
+        game.removeChild(document.getElementById(cactusId));
+    }, 1000);
+};
 
 const keyAction = (key) => {
     const keyCommand = {
@@ -22,7 +41,7 @@ const keyAction = (key) => {
 };
 
 const jump = (type) => {
-    if (dino.classList.length > 0) {
+    if (dino.classList.length > 2) {
         return;
     }
 
@@ -41,10 +60,62 @@ const jump = (type) => {
     const jumpProperties = typesProperties[type];
 
     dino.classList.add(jumpProperties.className);
+    dino.classList.remove("dinoRun");
 
     setTimeout(() => {
-        dino.classList.remove(jumpProperties.className);
+        if (dino.style.animationPlayState != "paused") {
+            dino.classList.remove(jumpProperties.className);
+            dino.classList.add("dinoRun");
+        }
     }, jumpProperties.timing);
+};
+
+const isDinoColliding = () => {
+    let individualCheck = (cactus) => {
+        {
+            const coord = {
+                dinoLeft: parseInt(
+                    window.getComputedStyle(dino).getPropertyValue("left")
+                ),
+                dinoTop: parseInt(
+                    window.getComputedStyle(dino).getPropertyValue("top")
+                ),
+                dinoWidth: parseInt(
+                    window.getComputedStyle(dino).getPropertyValue("width")
+                ),
+                dinoHeight: parseInt(
+                    window.getComputedStyle(dino).getPropertyValue("height")
+                ),
+                cactusLeft: parseInt(
+                    window.getComputedStyle(cactus).getPropertyValue("left")
+                ),
+                cactusTop: parseInt(
+                    window.getComputedStyle(cactus).getPropertyValue("top")
+                ),
+                cactusWidth: parseInt(
+                    window.getComputedStyle(cactus).getPropertyValue("width")
+                ),
+            };
+
+            return !(
+                coord.cactusLeft + coord.cactusWidth < coord.dinoLeft ||
+                coord.dinoLeft + coord.dinoWidth < coord.cactusLeft ||
+                coord.dinoHeight + coord.dinoTop < coord.cactusTop
+            );
+        }
+    };
+    const allCactus = Array.from(document.getElementsByClassName("cactus"));
+    return allCactus.some((cactus) => individualCheck(cactus));
+};
+
+const gameOver = () => {
+    isGameOver = true;
+    getCactus().forEach((cactus) => {
+        dino.style.animationPlayState = "paused";
+        dino.classList.add("dead");
+        dino.classList.remove("alive", "dinoRun");
+        cactus.style.animationPlayState = "paused";
+    });
 };
 
 document.addEventListener("keydown", (event) => {
@@ -52,15 +123,17 @@ document.addEventListener("keydown", (event) => {
 });
 
 setInterval(() => {
-    let dinoTop = parseInt(
-        window.getComputedStyle(dino).getPropertyValue("top")
-    );
-
-    let cactusLeft = parseInt(
-        window.getComputedStyle(cactus).getPropertyValue("left")
-    );
-
-    if (cactusLeft < 54 && cactusLeft > 0 && dinoTop >= 140) {
-        alert("Game Over!");
+    if (isDinoColliding()) {
+        gameOver();
     }
-}, 10);
+}, 1);
+
+const CACTUS_SPAWN_CHANCE = 2 / 5;
+const SPAWN_INTERVAL = 400; // ms
+setInterval(() => {
+    if (Math.random() < CACTUS_SPAWN_CHANCE) {
+        placeCactus();
+    }
+}, SPAWN_INTERVAL);
+
+setInterval(() => {}, 100);
